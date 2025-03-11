e240afe121f9947c0beeeda46803be763b1aa859
Shashank-ray2053 <shashankray2053@gmail.com>
2025-03-11 18:14:33 +0545

    final commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/NepTech/', // <-- Add this line here
+  
   server: {
     host: "::",
     port: 8080,
   },
+  
   plugins: [
     react(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
